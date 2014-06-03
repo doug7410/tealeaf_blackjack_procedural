b44df603b95939ae389298573a644d289e9c2896
Alberto De Lucca <albertodelucca@gmail.com>
2014-06-03 02:26:33 -0430

    Read card in human format.

--- tealeaf_blackjack_procedural.rb
+++ tealeaf_blackjack_procedural.rb
@@ -26,6 +26,38 @@ deck.shuffle!
 # an Ace can count as either 1 or 11
 # the cards from 2 to 9 are valued at their face value
 # the 10, Jack, Queen and King are all value at 10
+
+def read_card(card)
+  suit = nil
+  face = nil
+
+  case card[0]
+  when 1 then face = "One"
+  when 2 then face = "Two"
+  when 3 then face = "Three"
+  when 4 then face = "Four"
+  when 5 then face = "Five"
+  when 6 then face = "Six"
+  when 7 then face = "Seven"
+  when 8 then face = "Eight"
+  when 9 then face = "Nine"
+  when 10 then face = "Ten"
+  when "J" then face = "Jack"
+  when "Q" then face = "Queen"
+  when "K" then face = "King"
+  when "A" then face = "Ace"
+  end
+
+  case card[1]
+  when "H" then suit = "Hearts"
+  when "D" then suit = "Diamonds"
+  when "S" then suit = "Spades"
+  when "C" then suit = "Clubs"
+  end
+
+  return "#{face} of #{suit}"
+end
+
 #
 # TURN PREP
 # ask player(s) to make a bet
@@ -43,8 +75,10 @@ player[:hand] << deck.pop
 # deal one card to dealer
 dealer[:hand] << deck.pop
 
-binding.pry
 # show first card of dealer
+puts "Dealer first card is #{read_card(dealer[:hand][0])}"
+binding.pry
+
 # if the dealer holds a blackjack it must check the player(s) hand(s) - if the player doesn't have a blackjack to tie with the dealer then the player loses the bet automatically 
 #
 # PLAYER(S) TURN (loop until all players have finished playing)
